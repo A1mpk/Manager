b43a0e7e11bfe3ea8e596fa0f45c5e320196aa03
A1mpk <68470931+A1mpk@users.noreply.github.com>
2020-09-16 22:34:39 -0400

    Update help.js

--- commands/Moderation/help.js
+++ commands/Moderation/help.js
@@ -5,13 +5,14 @@ module.exports = {
     description: "LIST OF COMMANDS",
     execute(message, args){
         const HelpEmbed = new Discord.MessageEmbed()
-        .setAuthor('Moderation Commands')
+        .setAuthor('LIST OF COMMANDS -')
         .setColor(15158332)
         .addField('ban', "Used to ban a member.")
         .addField('kick', "The command will kick a user.")
         .addField('avatar','Shows the avatar of the mentionned person or themself.')
         .addField('announce','Use to announce things you want to.')
         .addField('lock','Locks all the channels when a raid is about to occure.')
+        .addField('say','say command, duplicates text into a bots one.')
         if(message.member.hasPermission('ADMINISTRATOR')) return message.channel.send(HelpEmbed)
         const PermEm = new Discord.MessageEmbed()
         .setAuthor('PERMISSION ERRORS')
